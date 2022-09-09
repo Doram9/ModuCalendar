47d290f395ffca77175b7d8fb6ec85208322544d
Doram9 <dodo0207k@naver.com>
2022-09-09 17:15:17 +0900

    [#1] feature - create new Springboot module

---
+++ moira/src/test/java/com/example/moira/MoiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
