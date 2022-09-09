d2dbea6bf4673ed2f6191dc1a73f61ff46c715f1
Doram9 <dodo0207k@naver.com>
2022-09-09 17:15:17 +0900

    [#1] feature - create new Springboot module

---
+++ moira/src/main/java/com/example/moira/MoiraApplication.java
@@ -0,0 +1,13 @@
+package com.example.moira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoiraApplication.class, args);
+	}
+
+}
