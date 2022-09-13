67d1b543471e8871a2c8ad80a7d95371203d19b3
Doram9 <dodo0207k@naver.com>
2022-09-13 22:54:29 +0900

    [#5] feature : inMemory User 생성 및 권한 부여

--- moira/src/main/java/com/example/moira/conf/SpringSecurityConfig.java
+++ moira/src/main/java/com/example/moira/conf/SpringSecurityConfig.java
@@ -1,29 +1,49 @@
 package com.example.moira.conf;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration //Classfile을 설정파일로 사용하게 해주는 어노테이션
 @EnableWebSecurity //스프링시큐리티 활성화
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        String password = passwordEncoder().encode("1111");
+
+        auth.inMemoryAuthentication().withUser("user") .password("password").roles("USER");
+        auth.inMemoryAuthentication().withUser("user") .password("password").roles("MANAGER");
+        auth.inMemoryAuthentication().withUser("user") .password("password").roles("ADMIN");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
+
 
         /**
          * 어느 요청이건 인증로직 처리
          */
         http
-                .authorizeRequests()
+            .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("admin").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
-                ;
-
-        http
-                .formLogin()
-                ;
+        .and()
+            .formLogin();
     }
 }
