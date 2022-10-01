6666d7f96fa6877343da11cfe26d268a0d807ed2
Doram9 <76899162+Doram9@users.noreply.github.com>
2022-10-02 00:41:25 +0900

    Revert "Feature/#7"

--- moira/src/main/java/com/example/moira/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.moira.repository;
-
-import com.example.moira.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-
-}
